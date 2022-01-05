faf9848fbf5cb81a7c5bb08eec5610cc4058e9ff
Priyanshu Das <priyanshu.d@staderlabs.com>
2022-01-05 18:25:04 +0530

    made some fixes suggested by cargo clippy and compile warnings

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub(crate) const OFFCHAIN_TIMESTAMP_DETAILS: &'static str = "off_chain_timestamp_details";
-pub(crate) const OFF_CHAIN_METRICS_FOR_VALIDATOR: &'static str = "off_chain_metrics_for_validator";
-pub(crate) const OFF_CHAIN_STATE: &'static str = "off_chain_state";
-pub(crate) const OFF_CHAIN_VALIDATOR_IDX_MAPPING: &'static str = "off_chain_validator_idx_mapping";
+pub(crate) const OFFCHAIN_TIMESTAMP_DETAILS: &str = "off_chain_timestamp_details";
+pub(crate) const OFF_CHAIN_METRICS_FOR_VALIDATOR: &str = "off_chain_metrics_for_validator";
+pub(crate) const OFF_CHAIN_STATE: &str = "off_chain_state";
+pub(crate) const OFF_CHAIN_VALIDATOR_IDX_MAPPING: &str = "off_chain_validator_idx_mapping";
